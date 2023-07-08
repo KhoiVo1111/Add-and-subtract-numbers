5b0289c3d2974f2d51d6de80610e34c94de73fdb
VoKhoi <138974522+KhoiVo1111@users.noreply.github.com>
2023-07-08 16:58:51 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, n;
+
+main(){
+    cout<<"Add and subtract numbers";
+    cout<<endl;
+    cout<<"Enter numbers a and b";
+    cout<<endl;
+    cin>>a>>b;
+    cout<<endl;
+    cout<<"Enter math "<<endl;
+    cout<<"1: Paddition math: a+b";
+    cout<<endl;
+    cout<<"2: Subtraction math : a-b";
+    cout<<endl;
+    cout<<"3: Addition math: a.b";
+    cout<<endl;
+    cout<<"4: Division math: a:b";
+    cout<<endl;
+    cout<<"Select the calculation by selecting 1,2,3 or 4";
+    cout<<endl;
+
+    double P=a/b
+    ;cin>>n;
+    cout<<"--------------------";
+    cout<<endl;
+    ;cout<<"A number "<< a;
+    cout<<endl;
+    ;cout<<"B number "<< b;
+    cout<<endl;
+    cout<<"--------------------";
+    cout<<endl;
+
+
+    switch(n){
+        case 1:{
+            cout<<"Addition = "<<a+b;
+            break;
+            }
+            case 2:{
+                cout<<"Subtraction = "<<a-b;
+                break;
+            }
+                case 3:{
+                cout<<"Multiplication = "<<a*b;
+                    break;
+                     }
+                       default:
+                    cout<<"Divisioncout = ";<<P; }
+                    }
